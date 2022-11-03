bcdf7ea4bd7ee2f6c1c9525a10ca0325933d7b8c
zengchanghuan <zengchanghuan@gmail.com>
2022-11-03 18:17:51 +0800

    add pro

--- algorithm_stack/main.c
+++ algorithm_stack/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 typedef char Element;
+
 struct LNode {
     Element element;
     struct LNode *next;
@@ -15,21 +15,10 @@ void initStack(Node head) {
     head->next = NULL;
 }
 
-bool isEmpty(Node head) {
-    return head->next == NULL;
-}
-
-Element popStack(Node head){
-    Node top = head->next;
-    head->next = head->next->next;
-    Element element = top->element;
-    free(element);
-    return element;
-}
 bool pushStack(Node head, Element element) {
     Node node = malloc(sizeof(struct LNode));
     if (node == NULL) {
-        return false;
+        return 0;
     }
     node->next = head->next;
     node->element = element;
@@ -37,11 +26,21 @@ bool pushStack(Node head, Element element) {
     return true;
 }
 
+bool isEmpty(Node head) {
+    return head->next == NULL;
+}
+
+Element popStack(Node head) {
+    Node top = head->next;
+    head->next = head->next->next;
+    Element e = top->element;
+    free(top);
+    return e;
+}
+
 bool isValid(char *s) {
     unsigned long len = strlen(s);
-    if (len % 2 == 1) {
-        return false;
-    }
+    if (len % 2 == 1) return false;  //如果长度不是偶数，那么一定不能成功匹配
     struct LNode head;
     initStack(&head);
     for (int i = 0; i < len; ++i) {
@@ -49,27 +48,20 @@ bool isValid(char *s) {
         if (c == '(' || c == '[' || c == '{') {
             pushStack(&head, c);
         } else {
-            if (isEmpty(&head)) {
-                return false;
-            }
+            if (isEmpty(&head)) return false;
             if (c == ')') {
-                if (popStack(&head) != '(') {
-                    return false;
-                }
+                if (popStack(&head) != '(') return false;
             } else if (c == ']') {
-                if (popStack(&head) != '[') {
-                    return false;
-                }
+                if (popStack(&head) != '[') return false;
             } else {
-                if (popStack(&head) != '{') {
-                    return false;
-                }
+                if (popStack(&head) != '{') return false;
             }
         }
     }
     return isEmpty(&head);
 }
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+
+
+
+
+
